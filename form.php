954eeb0715f115c241e7caa239f5432c0df45420
Amit Tallapragada <amittallapragada@gmail.com>
2016-01-04 15:30:21 -0800

    Tikas Praveen
    I deleted a lot of stuff and updated other stuff

--- form.php
+++ form.php
@@ -1,27 +1,26 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="Form_Style.css">
+	<link rel="stylesheet" type="text/css" href="form_style.css">
 </head>
 
 <h1>Create A Debater</h1>
-
+ 
 <body>
-<form action ="Translator.php" method="post"/>
-<p>
-Full Name: <input type="text" name="input1"/>
-Email: <input type="text" name="input2"/>
-Phone: <input name="input3" type="text" size="20">
-Number of Tournaments Attended: <input name="input4" type="text" size="20">
-Number of Tournaments Judged: <input name="imput5" type="text" size="20">
-Number of Wins: <input name="input6" type="text" size="20">
-Number of Losses: <input name="input7" type="text" size="20">
-Parent Name: <input name="input8" type="text" size="20">
-Parent Email: <input name="input9" type="text" size="20">
-</p>
+
+<form action ="translator.php" method="POST">
+Full Name: <br><input type="text" name="input1"><br>
+Email: <br><input type="text" name="input2"><br>
+Phone: <br><input type="text" name="input3"><br>
+Number of Tournaments Attended: <br><input type="text" name="input4"><br>
+Number of Tournaments Judged: <br><input type="text" name="input5"><br>
+Number of Wins: <br><input type="text" name="input6"><br>
+Number of Losses: <br><input type="text" name="input7"><br>
+Parent Name: <br><input type="text" name="input8"><br>
+Parent Email: <br><input type="text" name="input9"><br><br>
 <input type ="submit" value="Submit"/>
 </form>
 
 
 </body>
 </html>
+
