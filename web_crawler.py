ebc78392fcf4b1a2286e07e24b419ae2bb6c94b4
Arvind Sankar <arivndsanka97@gmail.com>
2016-01-03 17:24:23 -0800

    Gets the number of Wins from the tabor website.
    vikas naveen

---
+++ web_crawler.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+title = []
+
+def page_crawler():
+    url = 'https://www.tabroom.com/index/tourn/results/ranked_list.mhtml?event_id=28523&tourn_id=3024'
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "html.parser")
+    # add all numbers to list:
+    for link in soup.findAll({'td':'smallish centeralign'}):
+    	string = link.string
+        if string:
+        	for i in range(len(string) + 1):
+        		c = string[i-1:i]
+        		try:
+        			title.append(int(c))
+        		except ValueError:
+        			pass
+    print(title) #testing
+    fx = open("debate.txt", "w")
+    fx.write(str(title))
+    fx.close()
+    """except UnboundLocalError:
+        print('wtf')"""
+
+def delete_text():
+    for i in title:
+       if i == ("None"):
+           title.remove(i)
+
+
+
+page_crawler()
+delete_text()
