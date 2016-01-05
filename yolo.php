70bf36034ea6b5288a72e0515963ddda794776cb
vkas15 <vkas15@gmail.com>
2016-01-04 22:32:35 -0800

    changes to website

--- yolo.php
+++ yolo.php
@@ -1,68 +1,31 @@
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="Form_Style.css">
-</head>
+ <?php
+define('DB_NAME', 'forms1');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'Amit12');
+define('DB_HOST', 'localhost');
 
-<h1>Student Tournament Registration</h1>
- 
-<body>
+$link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
 
-<form action ="tournament_database.php" method="POST">
-Full Name: <br><input type="text" name="full_name"><br>
-Tournament Name: <br><input type="text" name="tournament_name"><br>
-Would you like to hire a judge:
-<br><input type="checkbox" name="judge_hired_option" value="Yes">Yes<br>
-<br><input type="checkbox" name="judge_hired_option" value="No" checked>No<br><br>
-<input type ="submit" value="Submit"/>
-</form>
+$db_selected = mysql_select_db(DB_NAME, $link);
 
-</body>
-</html>
+echo 'Connected Successfully';
 
+$name = ['x','y', 'z', 'a', 'b'];
+$namestr = implode("+", 'name');
 
-<?php
-$link = mysql_connect('localhost', 'root', 'Amit12');
-mysql_select_db('forms1', $link);
-
-$sql="SELECT * FROM tournament_registration";
-
-$records = mysql_query($sql);
-
-?>
-
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="table_style.css">
-<title>Tournament Data</title>
-</head>
-
-<body>
-<h1>Tournament Registration Information</h1>
-<table width="1000" border="1" cellpadding="1" cellspacing="1">
-<tr>
-
-<th>Tournament Name</th>
-<th>Student Name</th>
-<th> Student Tournament Name</th>
-<th> Hiring A Judge</th>
-<th> Amount of Judges Needed</th>
-<tr>
-
-<?php
-
-    while($student=mysql_fetch_assoc($records)) {
+    $j = 0; // counter
+    while (5 > $j) 
+    {
         echo "<tr>";
-        echo "<td>".$student['tournament_name']."</td>";
-        echo "<td>".$student['full_name']."</td>";
-        echo "<td>".$student['student_tournament_name']."</td>";
-        echo "<td>".$student['judge_hired_option']."</td>";
-        echo "<td>".$student['judges_needed']."</td>";
-        echo "</tr>";        
-    }//end while
-
-?>
-
-</table>
-</body>
-</html>
-
+        for ($i = 0; $i < 5; $i++) 
+        {
+              if(5 < $j) 
+                break; //exit from the loop if names are over
+        ?>
+          <td width="260px"><a href="<?php echo $link;?>" target="_blank" title="<?php echo $namestr; ?>"><?php echo $name; ?></a></td>
+        <?php
+            $j++;
+        }
+        echo "</tr>";
+    }
+    ?>
