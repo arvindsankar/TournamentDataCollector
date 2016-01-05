597bcb6be774b8d3e65ba5da3e8ec24ba31aad11
Arvind Sankar <arivndsanka97@gmail.com>
2016-01-04 19:28:22 -0800

    Geekas Gaveen

--- translator.php
+++ translator.php
@@ -8,9 +8,6 @@ $link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
 
 $db_selected = mysql_select_db(DB_NAME, $link);
 
-echo 'Connected Successfully';
-
-
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
@@ -18,17 +15,24 @@ $attend = $_POST['attend'];
 $judged = $_POST['judged'];
 $wins = $_POST['wins'];
 $loss = $_POST['loss'];
-$parent_phone = $_POST['parent_phone'];
+$parent_name = $_POST['parent_name'];
 $parent_email = $_POST['parent_email'];
 
 
-
-$sql ="INSERT INTO demo (name, email, phone, attend, judged, wins, loss, parent_phone, parent_email) VALUES ('$name', '$email', '$phone','$attend','$judged','$wins','$loss','$parent_phone','$parent_email')";
+$sql = "INSERT INTO demo (name, email, phone, attend, judged, wins, loss, parent_name, parent_email) VALUES ('$name', '$email', '$phone','$attend','$judged','$wins','$loss','$parent_name','$parent_email')";
 
 
 
 if($sql){
-	print("Success!");
+	$query = "SELECT * FROM demo";
+	$cat= mysql_query($query) or die(mysql_error());
+
+	while($row = mysql_fetch_assoc($cat)){
+	    foreach($row as $cname => $cvalue){
+	        print "$cname: $cvalue\t";
+	    }
+	    print "\n";
+	}
 }
 
 if (!mysql_query($sql)) {
