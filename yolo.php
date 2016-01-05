0d7e95007e0fce743b0e17f70882e588e1fba08e
Amit Tallapragada <amittallapragada@gmail.com>
2016-01-04 22:09:51 -0800

    Hkas Kveen
    Yesss

--- yolo.php
+++ yolo.php
@@ -1,3 +1,25 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Form_Style.css">
+</head>
+
+<h1>Student Tournament Registration</h1>
+ 
+<body>
+
+<form action ="tournament_database.php" method="POST">
+Full Name: <br><input type="text" name="full_name"><br>
+Tournament Name: <br><input type="text" name="tournament_name"><br>
+Would you like to hire a judge:
+<br><input type="checkbox" name="judge_hired_option" value="Yes">Yes<br>
+<br><input type="checkbox" name="judge_hired_option" value="No" checked>No<br><br>
+<input type ="submit" value="Submit"/>
+</form>
+
+</body>
+</html>
+
+
 <?php
 $link = mysql_connect('localhost', 'root', 'Amit12');
 mysql_select_db('forms1', $link);
@@ -10,11 +32,12 @@ $records = mysql_query($sql);
 
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="table_style.css">
 <title>Tournament Data</title>
 </head>
 
 <body>
-
+<h1>Tournament Registration Information</h1>
 <table width="1000" border="1" cellpadding="1" cellspacing="1">
 <tr>
 
@@ -42,3 +65,4 @@ $records = mysql_query($sql);
 </table>
 </body>
 </html>
+
