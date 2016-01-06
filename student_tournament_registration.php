b034f123e9cd6cf4a63d8e45af230606559908d6
Amit Tallapragada <amittallapragada@gmail.com>
2016-01-05 16:09:11 -0800

    5kas 5veen
    cleared and restored

--- student_tournament_registration.php
+++ student_tournament_registration.php
@@ -1,16 +1,8 @@
- <?php
-define('DB_NAME', 'forms1');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'Amit12');
-define('DB_HOST', 'localhost');
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Form_Style.css">
+</head>
 
-<<<<<<< HEAD:yolo.php
-$link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
-
-$db_selected = mysql_select_db(DB_NAME, $link);
-
-echo 'Connected Successfully';
-=======
     <h1>Student Tournament Registration</h1>
      
     <body>
@@ -28,29 +20,40 @@ echo 'Connected Successfully';
     </div>
 
 </html>
->>>>>>> 51d03e8664bf21b8f107f542ba83ca204867a43d:student_tournament_registration.php
 
-$name = ['x','y', 'z', 'a', 'b'];
-$namestr = implode("+", 'name');
 
-    $j = 0; // counter
-    while (5 > $j) 
-    {
+<?php
+$link = mysql_connect('localhost', 'root', 'Amit12');
+mysql_select_db('forms1', $link);
+
+$sql="SELECT * FROM tournament_registration";
+
+$records = mysql_query($sql);
+
+?>
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="table_style.css">
+<title>Tournament Data</title>
+</head>
+
+<body>
+<h1>Tournament Registration Information</h1>
+<table width="1000" border="1" cellpadding="1" cellspacing="1">
+<tr>
+
+<th>Tournament Name</th>
+<th>Student Name</th>
+<th> Student Tournament Name</th>
+<th> Hiring A Judge</th>
+<th> Amount of Judges Needed</th>
+<tr>
+
+<?php
+
+    while($student=mysql_fetch_assoc($records)) {
         echo "<tr>";
-<<<<<<< HEAD:yolo.php
-        for ($i = 0; $i < 5; $i++) 
-        {
-              if(5 < $j) 
-                break; //exit from the loop if names are over
-        ?>
-          <td width="260px"><a href="<?php echo $link;?>" target="_blank" title="<?php echo $namestr; ?>"><?php echo $name; ?></a></td>
-        <?php
-            $j++;
-        }
-        echo "</tr>";
-    }
-    ?>
-=======
         echo "<td>".$student['student_tournament_name']."</td>";
         echo "<td>".$student['full_name']."</td>";
         echo "<td>".$student['tournament_name']."</td>";
@@ -65,4 +68,3 @@ $namestr = implode("+", 'name');
 </body>
 </html>
 
->>>>>>> 51d03e8664bf21b8f107f542ba83ca204867a43d:student_tournament_registration.php
---
+++ tournament__database.php
@@ -0,0 +1,33 @@
+<?php
+define('DB_NAME', 'forms1');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'Amit12');
+define('DB_HOST', 'localhost');
+
+$link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
+
+$db_selected = mysql_select_db(DB_NAME, $link);
+
+echo 'Connected Successfully';
+
+
+$value1 = $_POST['tournament_name'];
+$value2 = $_POST['judges_needed'];
+$value3 = $_POST['full_name'];
+$value4 = $_POST['student_tournament_name'];
+$value5 = $_POST['judge_hired_option'];
+
+$sql ="INSERT INTO tournament_registration (tournament_name, full_name, student_tournament_name, judge_hired_option, judges_needed) VALUES ('$value1', '$value2', '$value3','$value4','$value5')";
+
+
+if($sql){
+	print("Success!");
+}
+
+if (!mysql_query($sql)) {
+	die('Error');
+}
+
+mysql_close();
+
+?>
