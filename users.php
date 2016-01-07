f2e0b15888a19f5035bf3c9df9ebd0f5331f8130
Amit Tallapragada <amittallapragada@gmail.com>
2016-01-06 18:20:31 -0800

    Trykas Tryveen
    sign up form

---
+++ users.php
@@ -0,0 +1,44 @@
+   <?php
+   	define('DB_NAME', 'forms1');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', 'Amit12');
+	define('DB_HOST', 'localhost');
+
+	$link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
+
+	$db_selected = mysql_select_db(DB_NAME, $link);
+
+
+	$coach = $_POST['coach'];
+	$school = $_POST['school'];
+	$email = $_POST['email'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$confirm = $_POST['confirm'];
+    $con=mysqli_connect("localhost","root","Amit12","forms1");
+    // Check connection
+    if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+
+    $result = mysqli_query($con,"SELECT username,email FROM users");
+
+    if($confirm != $password){
+    	die("Fuck no!");
+    }
+    while($row = mysqli_fetch_array($result))
+      {
+     	 if ($row['username'] == $username or $row['email'] == $email) {
+     	 	die('Fuck No!');
+     	 }
+      }
+	
+	#header("Location: website.html");
+	$sql = "INSERT INTO users (coach, school, email, username, password, confirm) VALUES ('$coach', '$school', '$email','$username','$password','$confirm')";
+	if(!mysql_query($sql)){
+		die('error!');
+	}    
+
+    mysqli_close($con);
+    ?>
