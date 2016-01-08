f88332590e838f46bab265e6799b7e1b71c37df2
Arvind Sankar <arivndsanka97@gmail.com>
2016-01-07 23:52:20 -0800

    I am the master
    Me

--- translator.php
+++ translator.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_NAME', 'forms1');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'Amit12');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
 $link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
@@ -17,7 +17,6 @@ $wins = $_POST['wins'];
 $loss = $_POST['loss'];
 $parent_name = $_POST['parent_name'];
 $parent_email = $_POST['parent_email'];
-
 
 $sql = "INSERT INTO demo (name, email, phone, attend, judged, wins, loss, parent_name, parent_email) VALUES ('$name', '$email', '$phone','$attend','$judged','$wins','$loss','$parent_name','$parent_email')";
 
